4f7e2a2d9fe7bea54898c764abde67e1f95eae04
Thomas 2 <thomas2@macbook-pro-de-thomas-4.home>
2018-01-16 23:18:34 +0100

    Simon dit

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
-#write your code here
+def echo (sentence)
+
+	return sentence
+end	
+
+#--------------------------
+
+def shout (sentence)
+
+	return sentence.upcase
+end
+
+#--------------------------
+
+def repeat (sentence, n_of_times=2)
+		
+		txt = sentence
+		n_of_times -= 1
+		n_of_times.times { txt += " #{sentence}"}
+
+		return txt
+end
+
+#--------------------------
+
+def start_of_word (word,n) #n le nombre de charactere
+
+	n = n - 1
+	word [0..n]
+
+end
+
