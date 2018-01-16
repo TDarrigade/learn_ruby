42f0fd1c5193c0b848d4349b2df3fcf91ebb25ce
Thomas 2 <thomas2@pc-84.home>
2018-01-16 14:45:13 +0100

    02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
 #write your code here
+calculator=0
+def add(x,y) 
+	 add = x + y
+end
+
+def subtract (x,y)
+	subtract = x - y
+end
+
+def sum(arr)
+    sum = 0
+    arr.each { |a| sum += a }
+    sum
+end
